e5c6a50382a69005a479d29c113f9b7cbb615d03
tristan-north <git@tristan-north.com>
2025-11-09 22:50:35 +1100

    Working on Day01 Part Two.

--- solutions/day01.dart
+++ solutions/day01.dart
@@ -2,12 +2,23 @@ import 'dart:io';
 import 'dart:convert';
 
 void main() {
-  final input = File('input/day01_input.txt').readAsStringSync();
+  final input = File('input/day01_example.txt').readAsStringSync();
   final lines = LineSplitter.split(input);
 
-  final sum = lines.fold(0, (sum, line) => sum + getNumberFromLine(line));
+  // {
+  //   final sum = lines.fold(0, (sum, line) => sum + getNumberFromLine(line));
+  //   print('Part One solution: $sum');
+  // }
 
-  print('Part One solution: $sum');
+  getNumberInclStringFromLine(lines.first);
+  // lines.forEach(getNumberInclStringFromLine);
+  // {
+  //   final sum = lines.fold(
+  //     0,
+  //     (sum, line) => sum + getNumberInclStringFromLine(line),
+  //   );
+  //   print('Part Two solution: $sum');
+  // }
 }
 
 int getNumberFromLine(String line) {
@@ -18,4 +29,18 @@ int getNumberFromLine(String line) {
   final last = chars.lastWhere(isNumber);
 
   return int.parse('$first$last');
+}
+
+enum nums { one, two, three, four, five, six, seven, eight, nine }
+
+int getNumberInclStringFromLine(String line) {
+  print(line);
+  for (final n in nums.values) {
+    final matchIndex = line.indexOf(n.name);
+    if (line.allMatches(n.name).firstOrNull != null) {
+    //   print('${n.name}');
+    // }
+  }
+
+  return 0;
 }
