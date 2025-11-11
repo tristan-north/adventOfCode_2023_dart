13ebc65e375c3eacc037c63a4ee07cbd0849d0d9
Tristan North <git@tristan-north.com>
2025-11-11 21:30:12 +1100

    Work on Day02.

--- solutions/day02.dart
+++ solutions/day02.dart
@@ -1,15 +1,17 @@
 import 'dart:io';
 
+const cubeLimits = (red: 12, green: 13, blue: 14);
+
 void main() {
   final input = File('input/day02_example.txt').readAsLinesSync();
 
-  input.forEach(print);
+  // Next add up the game numbers which pass and try making
+  // a big filter pipe.
 
   input.forEach((line) {
     final gameDataStr = line.split(':').skip(1).first;
 
-    final rounds = gameDataStr.split(';');
-
+    // TODO: Fix this abomination
     final gameData = gameDataStr
         .split(';')
         .map((n) => n.split(',').map((j) => j.trim()).toList().toList())
@@ -17,7 +19,22 @@ void main() {
 
     print('$gameData');
 
-    // Next create a function to take a string of a number and color
-    // and do something useful with it?
+    final valid = gameData.expand((x) => x).every((x) => withinCubeLimit(x));
+    print('valid: $valid');
   });
+}
+
+bool withinCubeLimit(String str) {
+  final [nChr, color] = str.split(' ');
+
+  final n = int.parse(nChr);
+
+  switch (color) {
+    case 'red' when n > cubeLimits.red:
+    case 'green' when n > cubeLimits.green:
+    case 'blue' when n > cubeLimits.blue:
+      return false;
+  }
+
+  return true;
 }
