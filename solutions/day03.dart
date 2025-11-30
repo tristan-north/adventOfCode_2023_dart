77f9b43ee8ac85562063a9d2bc6443081ef20ca7
Tristan North <git@tristan-north.com>
2025-11-30 22:59:24 +1100

    Started day03

--- solutions/day03.dart
+++ solutions/day03.dart
@@ -1,6 +1,7 @@
-// Loop through each character. Digits accumulate into serialNumBuf. 
-// If any digit in the buffer is adjacent to a symbol, the 'symbolNear' flag is set to true.
-// Upon hitting a non-digit char, the buffer is appended to validSerialNumbers if the flag is set.
+// Loop through each character. Digits accumulate into serialNumBuf.
+// If any digit in the buffer is adjacent to a symbol, the 'symbolNear' flag is
+// set to true. Upon hitting a non-digit char, the buffer is appended to
+// validSerialNumbers if the flag is set.
 
 // validSerialNumbers = []
 
@@ -22,4 +23,40 @@
 
 //   serialNumBuf.append(c);
 
+import 'dart:io';
 
+void main() {
+  final validSerialNumbers = <String>[];
+
+  final input = File('input/day02_input.txt').readAsLinesSync();
+
+  for (final line in input) {
+    var serialNumBuf = '';
+    var symbolNear = false;
+
+    for (final c in line.split('')) {
+      final intVal = int.tryParse(c);
+      if (intVal == null) {
+        if (serialNumBuf.length > 0) {
+          // Just left a serial num
+
+          if (symbolNear == true) {
+            validSerialNumbers.add(serialNumBuf);
+          }
+
+          // Reset state
+          symbolNear = false;
+          serialNumBuf = '';
+        }
+        continue;
+      }
+      // Is a digit
+      else {
+        final adjacentChars = getAdjacentChars();
+        if (adjacent_chars.contains(symbol)) symbolNear = true;
+
+        serialNumBuf += (c);
+      }
+    }
+  }
+}
